b17d31943a11b1915e58ea289c9addb0879a41eb
Raul F. Sanchez Falcon <raulsf10@gmail.com>
2022-03-15 10:13:02 +0000

    tutorial finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DAW2-angular-tutorial-RFSF';
+  title = 'DAW2-RFSF-Tout of Heroes';
 }
